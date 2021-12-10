1fb7f272cfae3e5888743b24eb1170dc04af0d37
Tony Ly <tony.ly@rightmove.co.uk>
2021-12-10 18:46:34 +0000

    initial commit

---
+++ src/main/java/com/example/gcpspringbootexample/ExampleController.java
@@ -0,0 +1,19 @@
+package com.example.gcpspringbootexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+
+@RequestMapping("/example")
+@RestController
+public class ExampleController {
+
+    @GetMapping
+    public Map<String, String> getSomeStuff() {
+        return Map.of("name", "tony",
+                "age", "none of your business");
+    }
+}
---
+++ src/main/java/com/example/gcpspringbootexample/GcpSpringbootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcpspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpSpringbootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcpSpringbootExampleApplication.class, args);
+    }
+
+}
