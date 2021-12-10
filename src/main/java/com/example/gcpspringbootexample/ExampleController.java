97f0f3af46f5dda5046992d9264a0500aaed9600
Tony Ly <tony.ly@rightmove.co.uk>
2021-12-10 22:38:36 +0000

    Deploy homepage

--- src/main/java/com/example/gcpspringbootexample/ExampleController.java
+++ src/main/java/com/example/gcpspringbootexample/ExampleController.java
@@ -1,17 +1,20 @@
 package com.example.gcpspringbootexample;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 
-@RequestMapping("/example")
 @RestController
 public class ExampleController {
 
-    @GetMapping
+    @GetMapping("/")
+    public String getHomePage() {
+        return "<html><body><h1>Hello Tony</h1></body></html>";
+    }
+
+    @GetMapping("/example")
     public Map<String, String> getSomeStuff() {
         return Map.of(
                 "name", "tony",
